8782c3cdf4bd7ab47f1ac75bbb56fd662f0973e4
Nishant <nknishant91@gmail.com>
2023-05-09 11:13:29 +0530

    promises

---
+++ function25.js
@@ -0,0 +1,43 @@
+// var p = new Promise()
+// var babu_promise = new Promise(function(resolve,failed){
+//     var sum = 0;
+//     for(var i = 0;i<BigInt(2345678);i++){
+//         sum+=i
+//     }
+//     resolve(sum)
+
+// })
+
+
+// console.log("A")
+
+// babu_promise.then((res)=>{
+//     console.log(res)
+// },(error)=>{
+
+// })
+
+// console.log("B")
+
+
+
+// console.log("A")
+// var age = 16
+// var vote = new Promise(function(resolve,failed){
+//     if(age >= 18){
+//         resolve("Vote")
+//     }
+//     else{
+//         failed("Not Vote")
+//     }
+// })
+
+// vote.then((a)=>{console.log(a)},(b)=>{console.log(b)})
+// console.log("B")
+
+Promise.resolve()
+Promise.reject()
+Promise.race()
+Promise.all()
+Promise.any()
+Promise.allSettled()
